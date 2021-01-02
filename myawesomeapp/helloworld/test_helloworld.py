7bc4ea0a04805e6b4f60316d125700f300ef69c1
Mike Fiedler <miketheman@gmail.com>
2021-01-02 10:59:18 -0500

    test: remove tests for internals, only test public
    Since we want to encourage testing of the public interfaces only, we can
    remove the test scaffolds put into place during development since we
    have a single entry point to the package, which in turn exercises all
    the internals.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- myawesomeapp/helloworld/test_helloworld.py
+++ myawesomeapp/helloworld/test_helloworld.py
@@ -1,31 +1,14 @@
 """
 Test helloworld.
 
-In reality, you might not actually want to test every internal method like this,
-as testing the public interface is more useful than testing each individual
-function, as they all get exercised.
+Only test the exported public method, not the internals.
+
+Since every bit get exercised by the publicly exported function, we get 100%
+coverage without worrying about internal organization or implementations.
 """
-from . import _collector
-from . import _combiner
-from . import _runner
-from . import _phraser
+from . import main
 
 
-def test_collector():
-    """Asserts that the collector collects the parts"""
-    assert _collector.collect() == ["hello", "world"]
-
-
-def test_combiner_combine():
-    """Tests that the combiner combines with a comma-space"""
-    assert _combiner.combine(["a", "b"]) == "a, b"
-
-
-def test_phraser():
-    """Asserts tha tthe phraser turns any set of words into a phrase"""
-    assert _phraser.phrasify("hi there") == "Hi there."
-
-
-def test_runner_main():
+def test_main():
     """Tests for the main runner returns the correct string."""
-    assert _runner.main() == "Hello, world."
+    assert main() == "Hello, world."
